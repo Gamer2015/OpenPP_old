3391e0b9b75bb77d7af15a2e1fe85eaba6e8e78b
Gamer2015 <stefankr@inode.at>
2015-04-17 13:41:10 +0200

    AI Stuff

---
+++ Sources/Playground/AI/NeuralNetworks2-build/CMakeFiles/PlaygroundAINeuralNetworks2.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Stefan/Documents/GitHub/Openpp/Sources/Playground/AI/NeuralNetworks2/main.cpp" "C:/Users/Stefan/Documents/GitHub/Openpp/Sources/Playground/AI/NeuralNetworks2-build/CMakeFiles/PlaygroundAINeuralNetworks2.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "C:/Users/Stefan/Documents/GitHub/Openpp/Sources/include"
+  "C:/Users/Stefan/Documents/GitHub/Openpp/Sources/3rdParty/boost_1_57_0"
+  "C:/Users/Stefan/Documents/GitHub/Openpp/Sources/3rdParty/Box2D_v2.3.0/Box2D"
+  "C:/Users/Stefan/Documents/GitHub/Openpp/Sources/3rdParty/SDL2-2.0.3/include"
+  "C:/Users/Stefan/Documents/GitHub/Openpp/Sources/3rdParty/SFML-2.2/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ Sources/Playground/AI/NeuralNetworks3-build/CMakeFiles/PlaygroundAINeuralNetworks3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PlaygroundAINeuralNetworks3.dir/main.cpp.obj"
+  "C:/Users/Stefan/Documents/GitHub/Openpp/Sources/Playground/AI/NeuralNetworks3/PlaygroundAINeuralNetworks3.pdb"
+  "C:/Users/Stefan/Documents/GitHub/Openpp/Sources/Playground/AI/NeuralNetworks3/PlaygroundAINeuralNetworks3.exe"
+  "C:/Users/Stefan/Documents/GitHub/Openpp/Sources/Playground/AI/NeuralNetworks3/PlaygroundAINeuralNetworks3.exe.manifest"
+  "C:/Users/Stefan/Documents/GitHub/Openpp/Sources/lib/libPlaygroundAINeuralNetworks3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PlaygroundAINeuralNetworks3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
